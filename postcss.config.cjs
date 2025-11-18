c25a700ad2aaf6ae3427ed82e07a6b443b08c04b
resimator-shreya-ghimire <you@example.com>
2025-11-18 18:01:35 +0545

    Setup and login

---
+++ postcss.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: {
+    // 💡 This is the correct package name for Tailwind as of v4.x and most v3.x setups
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+}
---
+++ src/pages/FoodDetails.tsx
@@ -0,0 +1,13 @@
+import { useParams } from 'react-router-dom';
+
+const FoodDetails = () => {
+  const { id } = useParams();
+
+  
+
+  return (
+    <div>FoodDetails</div>
+  )
+}
+
+export default FoodDetails
