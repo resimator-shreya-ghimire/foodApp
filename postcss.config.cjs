876d8e41bd2fa35471c58dfe94ac7d8486ede7a4
resimator-shreya-ghimire <you@example.com>
2025-11-19 09:41:14 +0545

    Revert "Setup and login"
    This reverts commit c25a700ad2aaf6ae3427ed82e07a6b443b08c04b.

--- postcss.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  plugins: {
-    // 💡 This is the correct package name for Tailwind as of v4.x and most v3.x setups
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
-  },
-}
--- src/components/foot.data.ts
+++
@@ -1,7 +0,0 @@
-export type Food = {
-  id: number;
-  name: string;
-  category: string;
-  price: number;
-  isVegetarian: boolean;
-};
