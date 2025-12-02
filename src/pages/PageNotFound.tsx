1b9692d13b1ddb17ce1c8f28a4bc65f575210014
resimator-shreya-ghimire <you@example.com>
2025-12-02 12:10:44 +0545

    Page Not Found UI

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,5 +1,15 @@
+import { Button } from '@/components/button/Button'
+import { useNavigate } from 'react-router'
+
 const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="max-w-6xl h-screen mx-auto text-center py-20 flex flex-col items-center justify-center">
+      <h1 className="text-5xl font-bold mb-4">404</h1>
+      <p className="text-xl text-gray-600 mb-8">Page Not Found</p>
+      <Button label='Go Back Home' variant='primary' onClick={() => navigate('/')} />
+    </div>
+  );
 };
 
 export default PageNotFound;
